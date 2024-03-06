727b89f067a09d128c9002f186fbf8f1ce81a822
AntoCic <anto.cic.127@gmail.com>
2024-03-06 15:04:05 +0100

    BONUS 1 - BONUS 2

--- js/script.js
+++ js/script.js
@@ -4,20 +4,32 @@ console.log("- START -");
 //  - per i multipli di 3 stampi “Fizz” al posto del numero e
 //  - per i multipli di 5 stampi “Buzz” al post del numero.
 //  - Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
-
+const rowE = document.getElementById("row");
 const totalNumber = 100;
 for (let i = 1; i <= totalNumber; i++) {
-    let outcome="";
-    if ( i % 3 == 0) {
-        outcome += "fizz";   
-    }
-    if ( i % 5 == 0) {
-        outcome += "buzz";   
-    }
-    if (!outcome) {
-        outcome = i;
-    }
+  const col_autoE = document.createElement("div");
+  col_autoE.classList.add("col-auto");
 
-    console.log(outcome);
-    
+  const outcomeE = document.createElement("p");
+  outcomeE.classList.add("ms_box");
+
+  let outcome = "";
+  if (i % 3 == 0) {
+    outcome += "fizz";
+    outcomeE.classList.add("fizz_box");
+  }
+  if (i % 5 == 0) {
+    outcome += "buzz";
+    outcomeE.classList.add("buzz_box");
+  }
+  if (!outcome) {
+    outcome = String(i);
+  }
+
+  console.log(outcome);
+
+  outcomeE.innerHTML = outcome;
+
+  col_autoE.appendChild(outcomeE);
+  rowE.appendChild(col_autoE);
 }
