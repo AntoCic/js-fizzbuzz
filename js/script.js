e486555671bccee26aaec94a1f8527c2cd20d861
AntoCic <anto.cic.127@gmail.com>
2024-03-06 14:48:08 +0100

    ADDED: JS exercise

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ console.log("- START -");
 //  - per i multipli di 3 stampi “Fizz” al posto del numero e
 //  - per i multipli di 5 stampi “Buzz” al post del numero.
 //  - Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+const totalNumber = 100;
+for (let i = 1; i <= totalNumber; i++) {
+    let outcome="";
+    if ( i % 3 == 0) {
+        outcome += "fizz";   
+    }
+    if ( i % 5 == 0) {
+        outcome += "buzz";   
+    }
+    if (!outcome) {
+        outcome = i;
+    }
+
+    console.log(outcome);
+    
+}
